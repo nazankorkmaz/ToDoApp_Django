4e6c94db75f420aaa23b8958070592674d5e7ff1
Nazan Korkmaz <nazzankorkmazz@gmail.com>
2025-02-11 18:51:40 +0300

    admin.py duzenlendi.

--- ToDoApp/gorev/admin.py
+++ ToDoApp/gorev/admin.py
@@ -4,4 +4,12 @@ from django.contrib import admin
 
 from .models import Gorev
 
-admin.site.register(Gorev)
+#admin.site.register(Gorev)
+
+@admin.register(Gorev)
+class GorevAdmin(admin.ModelAdmin):
+    list_display = ("baslik","tanim","tamamlandiMi","tarih") #hangileri gozuksun listelemede
+    list_display_links = ("baslik",)#hangileri link olsun
+    list_filter =("baslik","tamamlandiMi")#sag taradfa filtreleme kısmı cıktı
+    list_editable = ("tamamlandiMi",) #checkbox oldu
+    search_fields = ("baslik","tanim","tarih") # ustte arama cubugu cıktı
