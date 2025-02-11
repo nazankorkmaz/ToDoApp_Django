6aa285ac139002e2d292ac227bde93051eaaebf5
Nazan Korkmaz <nazzankorkmazz@gmail.com>
2025-02-11 18:44:36 +0300

    index.html tasarimi yapildi.

--- ToDoApp/gorev/views.py
+++ ToDoApp/gorev/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+from .models import Gorev
+
 def gorevListesi(request):
+     
+     gorevler = Gorev.objects.all()
     
-     return render(request,'gorev/index.html')
+     return render(request,'gorev/index.html',{
+          'gorevler':gorevler
+     })
     #return HttpResponse(" gorevlere başlayalım..")
