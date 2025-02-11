84efdcc46b2fc744a10a9e8a4e3ef60de6c5d329
Nazan Korkmaz <nazzankorkmazz@gmail.com>
2025-02-11 16:01:57 +0300

    Gorev modeli olusturuldu.

--- ToDoApp/gorev/models.py
+++ ToDoApp/gorev/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from  django.contrib.auth.models import User
 
 # Create your models here.
+class Gorev(models.Model): #ORM -Object Relational Mapping
+    kullanici =  models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True) # her gorev bir kullaniciya ait, kullanici isilinirse gorevleri de silinsin (cascade), bos birakilabilir bu alan, formlarda da bos kalabilir bu alan
+    baslik = models.CharField(max_length=200)
+    tanim = models.TextField(null= True, blank=True) #veritabaninda ve formda bu alan bos kalabilir
+    tamamlandiMi = models.BooleanField(default=False)
+    tarih = models.DateTimeField(auto_now_add=True) #gorev olusturuldugunda otomatik dolar
+
+    def __str__(self):
+        return self.baslik
+    
+    class Meta: #nodelin ek yapilandirilmalarinin yapildigi ic sinif
+        ordering = ['tamamlandiMi'] #veritabaninda siralama yapildiginda sonuclar nasıl donsun
--- ToDoApp/gorev/urls.py
+++ ToDoApp/gorev/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path('',views.gorevListesi, name="gorevListesi"),
-}
+]
 #aaa
